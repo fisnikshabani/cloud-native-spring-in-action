23f91e3a15816958dc99bc43048d2063e7f0c064
Fisnik Shabani <fisnik.shabani@nionit.com>
2025-10-25 01:25:19 +0200

    Add externalized configuration and test data loading
    - Implement @ConfigurationProperties for polar.greeting configuration
    - Add PolarProperties class to externalize application settings
    - Update HomeController to use configurable greeting message
    - Add BookDataLoader component for loading test data on startup
    - Configure bootRun task to use testdata profile by default
    - Add spring-boot-configuration-processor for better IDE support

---
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,27 @@
+package com.polarbookshop.catalogservice.demo;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567890", "Polar Book", "Polar Author1", 10.0);
+        var book2 = new Book("1234567891", "Polar Book 2", "Polar Author2", 20.0);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
